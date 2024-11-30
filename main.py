85da27554529f1af1b252d3c9a47ca248d05ea02
Ilya Eryomenko <ilya@eryomenko.com>
2024-11-30 10:16:39 +0500

    reformat files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from window import Window
 from line import Line, Point
 from cell import Cell
 
+
 def main():
     win = Window(800, 600)
     test_cell = Cell(100, 100, 200, 200, win)
--- window.py
+++ window.py
@@ -1,5 +1,6 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
         self.width = width
@@ -10,7 +11,6 @@ class Window:
         self.canvas.pack()
         self.window_running = False
 
-
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
@@ -18,12 +18,11 @@ class Window:
     def draw_line(self, line, fill_color):
         line.draw(self.canvas, fill_color)
 
-
     def wait_for_close(self):
         self.window_running = True
         while self.window_running:
             self.redraw()
-    
+
     def close(self):
         self.window_running = False
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
